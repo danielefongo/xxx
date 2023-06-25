9f0ec59093f54daf1838f26e7c6d079601b4d721
danielefongo <danielefongo@gmail.com>
2023-06-25 16:38:04 +0200

    add neovim

--- home.nix
+++ home.nix
@@ -18,6 +18,10 @@
   home.packages = [
 	pkgs.btop
 	pkgs.neovim
+  pkgs.fzf
+  pkgs.nodejs_20
+  pkgs.ripgrep
+  pkgs.unzip
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -39,6 +43,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".config/nvim".source = ref/nvim;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
